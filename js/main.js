d010f2251d00f4d499edcc276b3d274b4a4cc221
Noah Kovacs <n.kovacs1@icloud.com>
2015-12-14 20:11:16 -0500

    Finishing work! W00T!!! Added media queries to make it responsive.
    Signed-off-by: Noah Kovacs <n.kovacs1@icloud.com>

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 /**
  * Created by noahk on 12/3/15.
  */
+(function($) {
+    var $window = $(window),
+        $img = $('#zoom'),
+        $vert = $('.vert');
+
+    function resize() {
+        if ($window.width() > 700) {
+            return $img.addClass('zoom') && $vert.addClass('vert');
+        }
+        $vert.removeClass('vert');
+        $img.removeClass('zoom');
+    }
+
+    $window
+        .resize(resize)
+        .trigger('resize');
+})(jQuery);
